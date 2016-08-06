e2678615ff87eff1201c8f8fac76d0e246f8b385
aa <aa@aa.com>
2016-08-06 19:44:51 +0800

    two LEDs are blinking implementation of the struct on the void pointer

--- RTOSDemo/main-blinky.c
+++ RTOSDemo/main-blinky.c
@@ -1,71 +1,71 @@
 /*
-    FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
-    All rights reserved
+ FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
+ All rights reserved
 
-    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+ VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
 
-    This file is part of the FreeRTOS distribution.
+ This file is part of the FreeRTOS distribution.
 
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
+ FreeRTOS is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License (version 2) as published by the
+ Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
 
-    ***************************************************************************
-    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
-    >>!   distribute a combined work that includes FreeRTOS without being   !<<
-    >>!   obliged to provide the source code for proprietary components     !<<
-    >>!   outside of the FreeRTOS kernel.                                   !<<
-    ***************************************************************************
+ ***************************************************************************
+ >>!   NOTE: The modification to the GPL is included to allow you to     !<<
+ >>!   distribute a combined work that includes FreeRTOS without being   !<<
+ >>!   obliged to provide the source code for proprietary components     !<<
+ >>!   outside of the FreeRTOS kernel.                                   !<<
+ ***************************************************************************
 
-    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
-    link: http://www.freertos.org/a00114.html
+ FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  Full license text is available on the following
+ link: http://www.freertos.org/a00114.html
 
-    ***************************************************************************
-     *                                                                       *
-     *    FreeRTOS provides completely free yet professionally developed,    *
-     *    robust, strictly quality controlled, supported, and cross          *
-     *    platform software that is more than just the market leader, it     *
-     *    is the industry's de facto standard.                               *
-     *                                                                       *
-     *    Help yourself get started quickly while simultaneously helping     *
-     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
-     *    tutorial book, reference manual, or both:                          *
-     *    http://www.FreeRTOS.org/Documentation                              *
-     *                                                                       *
-    ***************************************************************************
+ ***************************************************************************
+ *                                                                       *
+ *    FreeRTOS provides completely free yet professionally developed,    *
+ *    robust, strictly quality controlled, supported, and cross          *
+ *    platform software that is more than just the market leader, it     *
+ *    is the industry's de facto standard.                               *
+ *                                                                       *
+ *    Help yourself get started quickly while simultaneously helping     *
+ *    to support the FreeRTOS project by purchasing a FreeRTOS           *
+ *    tutorial book, reference manual, or both:                          *
+ *    http://www.FreeRTOS.org/Documentation                              *
+ *                                                                       *
+ ***************************************************************************
 
-    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
-    the FAQ page "My application does not run, what could be wrong?".  Have you
-    defined configASSERT()?
+ http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
+ the FAQ page "My application does not run, what could be wrong?".  Have you
+ defined configASSERT()?
 
-    http://www.FreeRTOS.org/support - In return for receiving this top quality
-    embedded software for free we request you assist our global community by
-    participating in the support forum.
+ http://www.FreeRTOS.org/support - In return for receiving this top quality
+ embedded software for free we request you assist our global community by
+ participating in the support forum.
 
-    http://www.FreeRTOS.org/training - Investing in training allows your team to
-    be as productive as possible as early as possible.  Now you can receive
-    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
-    Ltd, and the world's leading authority on the world's leading RTOS.
+ http://www.FreeRTOS.org/training - Investing in training allows your team to
+ be as productive as possible as early as possible.  Now you can receive
+ FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
+ Ltd, and the world's leading authority on the world's leading RTOS.
 
-    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
-    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
-    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+ http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+ including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+ compatible FAT file system, and our tiny thread aware UDP/IP stack.
 
-    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
-    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
+ http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
+ Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
 
-    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
-    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
-    licenses offer ticketed support, indemnification and commercial middleware.
+ http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
+ Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
+ licenses offer ticketed support, indemnification and commercial middleware.
 
-    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
-    engineered and independently SIL3 certified version for use in safety and
-    mission critical applications that require provable dependability.
+ http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+ engineered and independently SIL3 certified version for use in safety and
+ mission critical applications that require provable dependability.
 
-    1 tab == 4 spaces!
-*/
+ 1 tab == 4 spaces!
+ */
 
 /* 
  * This is a very simple demo that creates two tasks and one queue.  One task
@@ -76,7 +76,7 @@
  *
  * For a much more complete and complex example select either the Debug or
  * Debug_with_optimisation build configurations within the HEW IDE. 
-*/
+ */
 
 /* Hardware specific includes. */
 #include "iodefine.h"
@@ -100,171 +100,204 @@
 #define getRtosDelay(x)	(x/portTICK_PERIOD_MS)
 
 /* The number of items the queue can hold.  This is 1 as the receive task
-will remove items as they are added so the send task should always find the
-queue empty. */
+ will remove items as they are added so the send task should always find the
+ queue empty. */
 #define mainQUEUE_LENGTH						( 1 )
 
 /*
  * The tasks as defined at the top of this file.
  */
-static void prvQueueReceiveTask( void *pvParameters );
-static void prvQueueSendTask( void *pvParameters );
-static void prvQueueBlinkOtherLED( void *pvParameters );
+static void prvQueueReceiveTask(void *pvParameters);
+static void prvQueueSendTask(void *pvParameters);
+static void prvQueueBlinkOtherLED(void *pvParameters);
 
 /* The queue used by both tasks. */
 static QueueHandle_t xQueue = NULL;
 
 /*-----------------------------------------------------------*/
 
+typedef struct
+    {
+	int delay;
+	int portNum;
+    } LedBlinkDataT;
+
+static const LedBlinkDataT LedBlinkData1 =
+    {
+    300, 1
+    };
+static const LedBlinkDataT LedBlinkData2 =
+    {
+    400, 2
+    };
+
 int main(void)
-{
-extern void HardwareSetup( void );
+    {
+    extern void HardwareSetup(void);
 
-	/* Renesas provided CPU configuration routine.  The clocks are configured in
-	here. */
-	HardwareSetup();
-	
-	/* Turn all LEDs off. */
-	vParTestInitialise();
-	
-	/* Create the queue. */
-	xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( unsigned long ) );
+    /* Renesas provided CPU configuration routine.  The clocks are configured in
+     here. */
+    HardwareSetup();
 
-	if( xQueue != NULL )
+    /* Turn all LEDs off. */
+    vParTestInitialise();
+
+    /* Create the queue. */
+    xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof(unsigned long));
+
+    if (xQueue != NULL)
 	{
-		/* Start the two tasks as described at the top of this file. */
-		xTaskCreate( prvQueueReceiveTask, "Rx", configMINIMAL_STACK_SIZE, NULL, configQUEUE_RECEIVE_TASK_PRIORITY, NULL );
-		xTaskCreate( prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, NULL, configQUEUE_SEND_TASK_PRIORITY, NULL );
-		xTaskCreate( prvQueueBlinkOtherLED, "blinkLED", configMINIMAL_STACK_SIZE, 1, configQUEUE_BLINKLED_TASK_PRIORITY, NULL );
-		xTaskCreate( prvQueueBlinkOtherLED, "blinkLED2", configMINIMAL_STACK_SIZE, 2, configQUEUE_BLINKLED_TASK_PRIORITY, NULL );
+	/* Start the two tasks as described at the top of this file. */
+	xTaskCreate(prvQueueReceiveTask, "Rx", configMINIMAL_STACK_SIZE, NULL,
+		configQUEUE_RECEIVE_TASK_PRIORITY, NULL);
+	xTaskCreate(prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, NULL,
+		configQUEUE_SEND_TASK_PRIORITY, NULL);
+	xTaskCreate(prvQueueBlinkOtherLED, "blinkLED", configMINIMAL_STACK_SIZE,
+		(void*) &LedBlinkData1, configQUEUE_BLINKLED_TASK_PRIORITY,
+		NULL);
+	xTaskCreate(prvQueueBlinkOtherLED, "blinkLED2",
+		configMINIMAL_STACK_SIZE, (void*) &LedBlinkData2,
+		configQUEUE_BLINKLED_TASK_PRIORITY, NULL);
 
-		/* Start the tasks running. */
-		vTaskStartScheduler();
+	/* Start the tasks running. */
+	vTaskStartScheduler();
 	}
-	
-	/* If all is well we will never reach here as the scheduler will now be
-	running.  If we do reach here then it is likely that there was insufficient
-	heap available for the idle task to be created. */
-	for( ;; );
-}
+
+    /* If all is well we will never reach here as the scheduler will now be
+     running.  If we do reach here then it is likely that there was insufficient
+     heap available for the idle task to be created. */
+    for (;;)
+	;
+    }
 /*-----------------------------------------------------------*/
 
-static void prvQueueBlinkOtherLED( void *pvParameters )
-{
-	TickType_t xNextWakeTime;
-	int p = (int)((int*)pvParameters);
-		/* Initialise xNextWakeTime - this only needs to be done once. */
-		xNextWakeTime = xTaskGetTickCount();
+static void prvQueueBlinkOtherLED(void *pvParameters)
+    {
+    TickType_t xNextWakeTime;
+    LedBlinkDataT *ptrLedBlinkData = (LedBlinkDataT*) pvParameters;
+    /* Initialise xNextWakeTime - this only needs to be done once. */
+    xNextWakeTime = xTaskGetTickCount();
 
-		for( ;; )
-		{
-			/* Place this task in the blocked state until it is time to run again.
-			The block state is specified in ticks, the constant used converts ticks
-			to ms. */
-			vTaskDelayUntil( &xNextWakeTime, getRtosDelay(500)  );
-			vParTestToggleLED( p );
-
-		}
-}
-
-static void prvQueueSendTask( void *pvParameters )
-{
-TickType_t xNextWakeTime;
-const unsigned long ulValueToSend = 100UL;
-
-	/* Initialise xNextWakeTime - this only needs to be done once. */
-	xNextWakeTime = xTaskGetTickCount();
-
-	for( ;; )
+    for (;;)
 	{
-		/* Place this task in the blocked state until it is time to run again. 
-		The block state is specified in ticks, the constant used converts ticks
-		to ms. */
-		vTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
+	/* Place this task in the blocked state until it is time to run again.
+	 The block state is specified in ticks, the constant used converts ticks
+	 to ms. */
+	vTaskDelayUntil(&xNextWakeTime, getRtosDelay(ptrLedBlinkData->delay));
+	vParTestToggleLED(ptrLedBlinkData->portNum);
 
-		/* Send to the queue - causing the queue receive task to flash its LED.  0
-		is used so the send does not block - it shouldn't need to as the queue
-		should always be empty here. */
-		xQueueSend( xQueue, &ulValueToSend, 0 );
 	}
-}
+    }
+
+static void prvQueueSendTask(void *pvParameters)
+    {
+    TickType_t xNextWakeTime;
+    const unsigned long ulValueToSend = 100UL;
+
+    /* Initialise xNextWakeTime - this only needs to be done once. */
+    xNextWakeTime = xTaskGetTickCount();
+
+    for (;;)
+	{
+	/* Place this task in the blocked state until it is time to run again.
+	 The block state is specified in ticks, the constant used converts ticks
+	 to ms. */
+	vTaskDelayUntil(&xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS);
+
+	/* Send to the queue - causing the queue receive task to flash its LED.  0
+	 is used so the send does not block - it shouldn't need to as the queue
+	 should always be empty here. */
+	xQueueSend(xQueue, &ulValueToSend, 0);
+	}
+    }
 /*-----------------------------------------------------------*/
 
-static void prvQueueReceiveTask( void *pvParameters )
-{
-unsigned long ulReceivedValue;
+static void prvQueueReceiveTask(void *pvParameters)
+    {
+    unsigned long ulReceivedValue;
 
-	for( ;; )
+    for (;;)
 	{
-		/* Wait until something arives in the queue - this will block 
-		indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-		FreeRTOSConfig.h. */
-		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
+	/* Wait until something arives in the queue - this will block
+	 indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
+	 FreeRTOSConfig.h. */
+	xQueueReceive(xQueue, &ulReceivedValue, portMAX_DELAY);
 
-		/*  To get here something must have arrived, but is it the expected
-		value?  If it is, toggle the LED. */
-		if( ulReceivedValue == 100UL )
-		{
-			vParTestToggleLED( 0 );
-		}
+	/*  To get here something must have arrived, but is it the expected
+	 value?  If it is, toggle the LED. */
+	if (ulReceivedValue == 100UL)
+	    {
+	    vParTestToggleLED(0);
+	    }
 	}
-}
+    }
 /*-----------------------------------------------------------*/
 
-void vApplicationSetupTimerInterrupt( void )
-{
-	/* Enable compare match timer 0. */
-	MSTP( CMT0 ) = 0;
-	
-	/* Interrupt on compare match. */
-	CMT0.CMCR.BIT.CMIE = 1;
-	
-	/* Set the compare match value. */
-	CMT0.CMCOR = ( unsigned short ) ( ( ( configPERIPHERAL_CLOCK_HZ / configTICK_RATE_HZ ) -1 ) / 8 );
-	
-	/* Divide the PCLK by 8. */
-	CMT0.CMCR.BIT.CKS = 0;
-	
-	/* Enable the interrupt... */
-	_IEN( _CMT0_CMI0 ) = 1;
-	
-	/* ...and set its priority to the application defined kernel priority. */
-	_IPR( _CMT0_CMI0 ) = configKERNEL_INTERRUPT_PRIORITY;
-	
-	/* Start the timer. */
-	CMT.CMSTR0.BIT.STR0 = 1;
-}
+void vApplicationSetupTimerInterrupt(void)
+    {
+    /* Enable compare match timer 0. */
+    MSTP (CMT0) = 0;
+
+    /* Interrupt on compare match. */
+    CMT0.CMCR.BIT.CMIE = 1;
+
+    /* Set the compare match value. */
+    CMT0.CMCOR = (unsigned short) (((configPERIPHERAL_CLOCK_HZ
+	    / configTICK_RATE_HZ) - 1) / 8);
+
+    /* Divide the PCLK by 8. */
+    CMT0.CMCR.BIT.CKS = 0;
+
+    /* Enable the interrupt... */
+    _IEN (_CMT0_CMI0) = 1;
+
+    /* ...and set its priority to the application defined kernel priority. */
+    _IPR (_CMT0_CMI0) = configKERNEL_INTERRUPT_PRIORITY;
+
+    /* Start the timer. */
+    CMT.CMSTR0.BIT.STR0 = 1;
+    }
 /*-----------------------------------------------------------*/
 
 /* This function is explained by the comments above its prototype at the top
-of this file. */
-void vApplicationMallocFailedHook( void )
-{
-	for( ;; );
-}
+ of this file. */
+void vApplicationMallocFailedHook(void)
+    {
+    for (;;)
+	;
+    }
 /*-----------------------------------------------------------*/
 
 /* This function is explained by the comments above its prototype at the top
-of this file. */
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
-{
-	for( ;; );
-}
+ of this file. */
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
+    {
+    for (;;)
+	;
+    }
 /*-----------------------------------------------------------*/
 
 /* This function is explained by the comments above its prototype at the top
-of this file. */
-void vApplicationIdleHook( void )
-{
-}
+ of this file. */
+void vApplicationIdleHook(void)
+    {
+    }
 /*-----------------------------------------------------------*/
 
 /* The following four functions are here just to allow all three build 
-configurations to use the same vector table.  They are not used in this
-demo, but linker errors will result if they are not defined.  They can
-be ignored. */
-void vT0_1_ISR_Handler( void ) {}
-void vT2_3_ISR_Handler( void ) {}
-void vEMAC_ISR_Handler( void ) {}
-void vTimer2_ISR_Handler( void ) {}
+ configurations to use the same vector table.  They are not used in this
+ demo, but linker errors will result if they are not defined.  They can
+ be ignored. */
+void vT0_1_ISR_Handler(void)
+    {
+    }
+void vT2_3_ISR_Handler(void)
+    {
+    }
+void vEMAC_ISR_Handler(void)
+    {
+    }
+void vTimer2_ISR_Handler(void)
+    {
+    }
 volatile unsigned long ulHighFrequencyTickCount = 0;
